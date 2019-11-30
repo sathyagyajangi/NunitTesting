61f91ff8e09839b06ce47c846d7a7093b906d401
sathya <sathyagyajangidocs@gmail.com>
2019-11-30 11:50:00 +0530

    second

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,18 +1,25 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+
+            IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory);
+
+            driver.Url = "https://beta-cricket-yahoo.sportz.io/";
+
+            driver.Manage().Window.Maximize();
+
         }
     }
 }
